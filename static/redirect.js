b23ea7474c6c3d806dc5cc620fdfd13843e5d415
Daniel Micay <danielmicay@gmail.com>
2020-04-01 06:13:08 -0400

    use ES6 modules for all JavaScript

--- static/redirect.js
+++ static/redirect.js
@@ -1,7 +1,5 @@
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT
 
-"use strict";
-
 // Client-side redirects for fragments (anchors)
 //
 // It should be possible to do this with server-side redirects, but it was never implemented or
--- static/releases.js
+++ static/releases.js
@@ -1,7 +1,5 @@
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT
 
-"use strict";
-
 const baseUrl = "https://releases.grapheneos.org/";
 const versionBaseUrl = "https://github.com/GrapheneOS/platform_manifest/releases/tag/";
 const devices = ["bonito", "sargo", "crosshatch", "blueline", "taimen", "walleye"];
