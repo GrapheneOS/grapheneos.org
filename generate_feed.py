abc83cbed6ebba15b5fc5a6f4f9524bb7e04db19
Daniel Micay <danielmicay@gmail.com>
2020-12-02 12:28:19 -0500

    generate atom feed for releases

---
+++ generate_feed.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+
+import lxml.html
+from lxml import etree
+
+document = lxml.html.parse("static_tmp/releases.html").getroot()
+releases = document.body.cssselect("#changelog h3")
+
+updated = None
+entries = []
+
+for release in releases:
+    title = release.attrib["id"]
+    time = datetime.strptime(title, "%Y.%m.%d.%H").isoformat() + "Z"
+    if updated is None:
+        updated = time
+    content = []
+    element = release.getnext()
+    while element is not None and element.tag != "h3":
+        content.append(etree.tostring(element).decode())
+        element = element.getnext()
+    entry = f"""\
+    <entry>
+        <id>https://grapheneos.org/releases#{title}</id>
+        <link href="https://grapheneos.org/releases#{title}"/>
+        <title>{title}</title>
+        <updated>{time}</updated>
+        <content type="xhtml">
+            <div xmlns="http://www.w3.org/1999/xhtml">
+                {"".join(content)}
+            </div>
+        </content>
+    </entry>"""
+    entries.append(entry)
+
+feed = f"""<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <id>https://grapheneos.org/releases#changelog</id>
+    <link href="http://grapheneos.org/releases#changelog"/>
+    <link rel="self" href="https://grapheneos.org/releases.atom"/>
+    <title>GrapheneOS changelog</title>
+    <updated>{updated}</updated>
+    <author>
+        <name>GrapheneOS</name>
+        <email>contact@grapheneos.org</email>
+        <uri>https://grapheneos.org/</uri>
+    </author>
+    {"".join(entries)}
+</feed>
+"""
+
+with open("static_tmp/releases.atom", "w") as f:
+    f.write(feed)
