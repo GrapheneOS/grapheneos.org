aae0ac8edf404d47c664506e26f8a23e8a36f01d
Daniel Micay <danielmicay@gmail.com>
2021-03-06 22:38:39 -0500

    replace legacy /install redirect

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -22,6 +22,27 @@ const redirects = new Map([
     ["/build#upgrading-to-android-10", "/build#generating-release-signing-keys"],
     ["/install/cli#fastboot-as-non-root", "/install/cli#flashing-as-non-root"],
     ["/install/web#fastboot-as-non-root", "/install/web#flashing-as-non-root"],
+
+    // preserve links to CLI install guide from when it was /install
+    ["/install/#prerequisites", "/install/cli#prerequisites"],
+    ["/install/#enabling-oem-unlocking", "/install/cli#enabling-oem-unlocking"],
+    ["/install/#opening-terminal", "/install/cli#opening-terminal"],
+    ["/install/#obtaining-fastboot", "/install/cli#obtaining-fastboot"],
+    ["/install/#standalone-platform-tools", "/install/cli#standalone-platform-tools"],
+    ["/install/#checking-fastboot-version", "/install/cli#checking-fastboot-version"],
+    ["/install/#fastboot-as-non-root", "/install/cli#fastboot-as-non-root"],
+    ["/install/#connecting-phone", "/install/cli#connecting-phone"],
+    ["/install/#unlocking-the-bootloader", "/install/cli#unlocking-the-bootloader"],
+    ["/install/#obtaining-signify", "/install/cli#obtaining-signify"],
+    ["/install/#obtaining-factory-images", "/install/cli#obtaining-factory-images"],
+    ["/install/#flashing-factory-images", "/install/cli#flashing-as-non-root"],
+    ["/install/#troubleshooting", "/install/cli#troubleshooting"],
+    ["/install/#locking-the-bootloader", "/install/cli#locking-the-bootloader"],
+    ["/install/#post-installation", "/install/cli#post-installation"],
+    ["/install/#booting", "/install/cli#booting"],
+    ["/install/#disabling-oem-unlocking", "/install/cli#disabling-oem-unlocking"],
+    ["/install/#replacing-grapheneos-with-the-stock-os", "/install/cli#replacing-grapheneos-with-the-stock-os"],
+    ["/install/#further-information", "/install/cli#further-information"],
 ]);
 
 function handle_hash() {
