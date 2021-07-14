37ef8d15027fb00d672ebbed028e93f3741f7189
Daniel Micay <danielmicay@gmail.com>
2021-07-14 01:53:57 -0400

    remove Pixel 2 and Pixel 2 XL from releases list

--- static/js/releases.js
+++ static/js/releases.js
@@ -2,7 +2,7 @@
 
 const baseUrl = "https://releases.grapheneos.org/";
 const versionBaseUrl = "https://github.com/GrapheneOS/platform_manifest/releases/tag/";
-const devices = ["redfin", "bramble", "sunfish", "coral", "flame", "bonito", "sargo", "crosshatch", "blueline", "taimen", "walleye"];
+const devices = ["redfin", "bramble", "sunfish", "coral", "flame", "bonito", "sargo", "crosshatch", "blueline"];
 const channels = ["stable", "beta"];
 const delayMs = 1000 * 60 * 5;
 
