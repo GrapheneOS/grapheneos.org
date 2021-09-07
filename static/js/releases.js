31376df595d1e191d2f8847873194f2c011fbd2d
Daniel Micay <danielmicay@gmail.com>
2021-09-07 04:50:27 -0400

    launch experimental Pixel 5a support

--- static/js/releases.js
+++ static/js/releases.js
@@ -2,7 +2,7 @@
 
 const baseUrl = "https://releases.grapheneos.org/";
 const versionBaseUrl = "https://github.com/GrapheneOS/platform_manifest/releases/tag/";
-const devices = ["redfin", "bramble", "sunfish", "coral", "flame", "bonito", "sargo", "crosshatch", "blueline"];
+const devices = ["barbet", "redfin", "bramble", "sunfish", "coral", "flame", "bonito", "sargo", "crosshatch", "blueline"];
 const channels = ["stable", "beta"];
 const delayMs = 1000 * 60 * 5;
 
