1fb198297df4d8766a38a49b8cba85765788f76c
Daniel Micay <danielmicay@gmail.com>
2022-08-28 14:06:14 -0400

    update fastboot.js

--- static/js/web-install.js
+++ static/js/web-install.js
@@ -1,6 +1,6 @@
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT
 
-import * as fastboot from "./fastboot/dfcb7800/fastboot.min.mjs";
+import * as fastboot from "./fastboot/ce2370da/fastboot.min.mjs";
 
 const RELEASES_URL = "https://releases.grapheneos.org";
 
@@ -325,7 +325,7 @@ fastboot.setDebugLevel(2);
 
 fastboot.configureZip({
     workerScripts: {
-        inflate: ["/js/fastboot/dfcb7800/vendor/z-worker-pako.js", "pako_inflate.min.js"],
+        inflate: ["/js/fastboot/ce2370da/vendor/z-worker-pako.js", "pako_inflate.min.js"],
     },
 });
 
