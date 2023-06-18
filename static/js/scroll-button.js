7e000be6821a3b582bcdc909dd62de9860d0cef5
tedgravlin <123981212+tedgravlin@users.noreply.github.com>
2023-06-18 17:32:47 -0400

    Add a scroll to top button on long pages

---
+++ static/js/scroll-button.js
@@ -0,0 +1,29 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT
+
+// When the user scrolls
+window.onscroll = function () {
+  showButton()
+};
+
+// Shows or hides the scroll button
+function showButton() {
+  // Get the scroll button
+  const scrollButton = document.getElementById("scrollButton");
+
+  // When the user scrolls down, make the scroll button visible
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  }
+  // When the user is as the top of the page, hide the scroll button
+  else {
+    scrollButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the page
+function scrollUp() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
