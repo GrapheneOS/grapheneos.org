523dce859e3c5167cb048b4547ad99af2327ddd9
tedgravlin <123981212+tedgravlin@users.noreply.github.com>
2023-06-18 19:35:23 -0400

    Minify JavaScript code

--- static/js/scroll-button.js
+++ static/js/scroll-button.js
@@ -1,28 +1,2 @@
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT
-
-// When the user scrolls
-window.onscroll = function() {
-  showButton();
-};
-
-// Shows or hides the scroll button
-function showButton() {
-  // Get the scroll button
-  const scrollButton = document.getElementById("scroll-button");
-  // When the user scrolls down, make the scroll button visible
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      scrollButton.style.display = "block";
-  }
-  // When the user is as the top of the page, hide the scroll button
-  else {
-      scrollButton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the page
-function scrollUp() {
-  window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-  });
-}
+window.onscroll=function(){showButton()};function showButton(){const scrollButton=document.getElementById("scroll-button");if(document.body.scrollTop>200||document.documentElement.scrollTop>200){scrollButton.style.display="block"}else{scrollButton.style.display="none"}}function scrollUp(){window.scrollTo({top:0,behavior:"smooth"})}
