e0a6b5f948bfbb08f7e3f520bba9c86a64e95b7d
Daniel Micay <daniel.micay@grapheneos.org>
2023-06-20 16:45:49 -0400

    add tangorpro to web install internal lists

--- static/js/web-install.js
+++ static/js/web-install.js
@@ -146,9 +146,9 @@ const qualcommDevices = ["barbet", "redfin", "bramble", "sunfish", "coral", "fla
 
 const legacyQualcommDevices = ["sunfish", "coral", "flame"];
 
-const tensorDevices = ["lynx", "cheetah", "panther", "bluejay", "raven", "oriole"];
+const tensorDevices = ["tangorpro", "lynx", "cheetah", "panther", "bluejay", "raven", "oriole"];
 
-const day1SnapshotCancelDevices = ["lynx", "cheetah", "panther", "bluejay", "raven", "oriole", "barbet", "redfin", "bramble"];
+const day1SnapshotCancelDevices = ["tangorpro", "lynx", "cheetah", "panther", "bluejay", "raven", "oriole", "barbet", "redfin", "bramble"];
 
 async function getLatestRelease() {
     let product = await device.getVariable("product");
