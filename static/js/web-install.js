86fa45d3987bfabae6432c85eec2b1a7f6673c22
Daniel Micay <daniel.micay@grapheneos.org>
2023-07-26 12:12:29 -0400

    staging

--- static/js/web-install.js
+++ static/js/web-install.js
@@ -204,7 +204,7 @@ async function getLatestRelease() {
         throw new Error(`device model (${product}) is not supported by the GrapheneOS web installer`);
     }
 
-    let metadataResp = await fetch(`${RELEASES_URL}/${product}-stable`);
+    let metadataResp = await fetch(`${RELEASES_URL}/${product}-beta`);
     let metadata = await metadataResp.text();
     let releaseId = metadata.split(" ")[0];
 
